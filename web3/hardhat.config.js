a1c77d8b4b86dac784cc9a87bb7ae5423600e284
ulas96 <y.ulas.yildiz@gmail.com>
2023-12-02 11:23:58 +0300

    deploy.js update

---
+++ web3/hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
