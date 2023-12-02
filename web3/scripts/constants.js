d5b5e17cb8b8237b23d044ce85b584752039db33
ulas96 <y.ulas.yildiz@gmail.com>
2023-12-02 13:37:18 +0300

    personal address is added to exports

--- web3/scripts/constants.js
+++ web3/scripts/constants.js
@@ -1,2 +1,5 @@
-export const contractAddr = "0x25725A198311e4db07Eb522F9E3b46203f723A1d";
-export const contractName = "Uruk";
+const contractAddr = "0xab0CAD129b8425C621dD228f1e36C3C49F7fE5Bc";
+const contractName = "Uruk";
+const personalAddress = "0x002C65Be429d430DF090f2DC847df3b468676029";
+
+module.exports = {contractAddr, contractName, personalAddress};
