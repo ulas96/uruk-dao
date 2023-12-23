608330646ed7908e44c9027a67f4580dbaa38fbe
ulas96 <y.ulas.yildiz@gmail.com>
2023-12-23 14:59:02 +0300

    contract address is updated

--- client/src/contract/constants.js
+++ client/src/contract/constants.js
@@ -2,4 +2,4 @@ const contractAddr = "0x4Bb45c93De3d8b9BfbEd4b3e6Ae3Eb9d653E5f36";
 const contractName = "Uruk";
 const personalAddress = "0x002C65Be429d430DF090f2DC847df3b468676029";
 
-module.exports = {contractAddr, contractName, personalAddress};
+export {contractAddr, contractName, personalAddress};
