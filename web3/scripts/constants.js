ee2a0f39d5dade6b9dab09b55fa124739ee752b7
ulas96 <y.ulas.yildiz@gmail.com>
2023-12-28 14:55:24 +0300

    contract updated for nickname check

--- web3/scripts/constants.js
+++ web3/scripts/constants.js
@@ -1,4 +1,4 @@
-const contractAddr = "0x4Bb45c93De3d8b9BfbEd4b3e6Ae3Eb9d653E5f36";
+const contractAddr = "0x55d7A3B36E411B878c1017D30C02A9d1B84D84C1";
 const contractName = "Uruk";
 const personalAddress = "0x002C65Be429d430DF090f2DC847df3b468676029";
 
