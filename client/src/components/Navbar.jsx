dfaded3df349571f6a219704ac5e200cdfc7735e
ulas96 <y.ulas.yildiz@gmail.com>
2024-01-01 16:32:31 +0300

    Navbar elements are updated and connected account is added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,27 +1,33 @@
 import {logo} from "../constants/main.js";
 import "./style.css";
+import {Link} from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({member}) => {
 
     return (
-        <nav >
+        <nav>
             <img className="navbar-items" id="logo" src={logo} alt="logo"/>
             <ul className="navbar-list">
+
                 <li className="navbar-items">
-                    <div className="link"  >Home</div>
+                    <Link className="link" to="/community">Community</Link>
                 </li>
                 <li className="navbar-items">
-                    <div className="link"  >Community</div>
+                    <Link className="link" to="/feed">Feed</Link>
                 </li>
                 <li className="navbar-items">
-                    <div className="link"  >Feed</div>
-                </li>
-                <li className="navbar-items">
-                    <div className="link"  >Projects</div>
+                    <Link className="link" to="/profile">Profile</Link>
                 </li>
             </ul>
 
+            <div className="account">
+                <div className="account-address">
+                    <p>{member.memberAddress !== null ? `${member.nickname}` : "Connect Wallet"}</p>
+                    <p>{member.memberAddress !== null ? `${String(member.memberAddress).slice(0, 5)}...${String(member.memberAddress).slice(39, 42)}` : "" +
+                        ""}</p>
 
+                </div>
+            </div>
         </nav>
     );
 };
