76ea456cac901a9b50bb40a19a46aa5e0d71b19c
ulas96 <y.ulas.yildiz@gmail.com>
2024-02-06 01:01:42 +0300

    config update

--- client/src/components/Community.jsx
+++ client/src/components/Community.jsx
@@ -7,23 +7,7 @@ const Community = ({state}) => {
         memberAddress: null,
         memberSince: null,
     }]);
-    const [memberPosts, setMemberPosts] = useState([{
-        memberIndex: null,
-        memberAddress: null,
-        postIndex: null,
-        postContent: null,
-        postDate: null,
-        postLikes: null,
-        postComments: null,
-    }]);
-    const getMemberPosts = async (_memberAddr) => {
-        const memberPosts = state.contract.getMemberPosts(_memberAddr);
-        setMemberPosts(memberPosts);
-    }
-    const getMembers = async () => {
-        const _members = await state.contract.getMembers();
-        setMembers(_members);
-    }
+
 
 
 
