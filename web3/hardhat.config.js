7475204082d80503f7873b867b7ba289099c4503
ulas96 <y.ulas.yildiz@gmail.com>
2024-05-19 18:23:12 +0300

    Update hardhat.config.js

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -5,6 +5,9 @@ require("dotenv").config()
 module.exports = {
   solidity: "0.8.20",
   networks: {
+    hardhat: {
+      //chainId: 1337
+    },
     goerli: {
       url: process.env.GOERLI_URL,
       accounts: [process.env.PRIVATE_KEY]
