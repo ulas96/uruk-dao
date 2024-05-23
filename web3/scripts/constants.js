6642692ec16d9cf282873be96102867b75d7bc67
ulas96 <y.ulas.yildiz@gmail.com>
2024-05-23 07:53:43 +0300

    update

--- web3/scripts/constants.js
+++ web3/scripts/constants.js
@@ -1,6 +1,6 @@
-const contractAddr = "0x9C4577b3a6E179CbffDD7C6F08d7c0E4478928d8";
+const contractAddr = "0x112277Cf678e68DDFCFEfE06364692345AaD6F47";
 const contractName = "Uruk";
-const personalAddress = "0x002C65Be429d430DF090f2DC847df3b468676029"; //0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 personalAddress = ""; //0x002C65Be429d430DF090f2DC847df3b468676029
+const personalAddress = "0xfb65f16fBB9f02E4b41c648CAfC99479033eB65A"; //0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 personalAddress = ""; //0x002C65Be429d430DF090f2DC847df3b468676029
 
 
 module.exports = {contractAddr, contractName, personalAddress};
