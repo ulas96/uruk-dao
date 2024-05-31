ae63d5bd81e72198916270e1e69172747bee1312
ulas96 <y.ulas.yildiz@gmail.com>
2024-05-31 14:03:38 +0300

    update

--- web3/scripts/constants.js
+++ web3/scripts/constants.js
@@ -1,4 +1,4 @@
-const contractAddr = "0x112277Cf678e68DDFCFEfE06364692345AaD6F47";
+const contractAddr = "0xd9447c058938D8Fcf917cDBC3544f089D8e12Eb6";
 const contractName = "Uruk";
 const personalAddress = "0xfb65f16fBB9f02E4b41c648CAfC99479033eB65A"; //0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 personalAddress = ""; //0x002C65Be429d430DF090f2DC847df3b468676029
 
--- web3/scripts/publishArticle.js
+++ web3/scripts/publishArticle.js
@@ -3,19 +3,13 @@ const { contractAddr, contractName, personalAddress } = require("../scripts/cons
 const {bondHash, readHashBond} = require("proof-of-string");
 
 async function main() {
-
     const contract = await hre.ethers.getContractAt(contractName, contractAddr);
-    const hash = await bondHash("Uruk DAO is more advanced than ever")
+    const hash = await bondHash("Uruk DAO is more advanced than ever");
     const publishArticleTx = await contract.publishArticle(hash);
     await publishArticleTx.wait();
     const article = await contract.getArticle(1);
     const string = await readHashBond(article.content);
     console.log(string)
-}
+};
 
-main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+main();
