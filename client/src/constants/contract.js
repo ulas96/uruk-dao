3e626f11927b2c24903d42b1f1fb83a1b2d6f9ee
ulas96 <y.ulas.yildiz@gmail.com>
2024-06-17 12:56:44 +0300

    Update contract.js

--- client/src/constants/contract.js
+++ client/src/constants/contract.js
@@ -1,4 +1,4 @@
-const contractAddr = "0xDDF85847271c9dAF9Fd1c1C6e300ae6Ac88a9333";
+const contractAddr = "0xd9447c058938D8Fcf917cDBC3544f089D8e12Eb6";
 const contractName = "Uruk";
 const personalAddress = "0x002C65Be429d430DF090f2DC847df3b468676029";
 
